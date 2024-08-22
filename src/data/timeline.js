992fc7e322cd7a0548ce67ca6c4e94d9b6a03f78
Antonio Di Palma <dipalma.antonio98@gmail.com>
2024-08-22 16:32:32 +0200

    Started implementation

---
+++ src/data/timeline.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    year: 'Today',
+    title: 'Software Engineer',
+    duration: '2 years',
+    details: '',
+  },
+  {
+    year: '2022',
+    title: 'Intern',
+    duration: '3 months',
+    details: '',
+  },
+  {
+    year: '2019',
+    title: 'Bachelor Degree in Computer Engineering',
+    duration: '3 years',
+    details: '',
+  },
+  {
+    year: '2018',
+    title: 'World Class Developer Diploma',
+    duration: '1 years',
+    details: '',
+  },
+  {
+    year: '2012',
+    title: 'Discovering Coding and Programming',
+    duration: 'where everything started',
+    details: '',
+  },
+];
