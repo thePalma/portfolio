4c1519b99fe4bfa35412365f6ab0acd2458cdf3c
thePalma <dipalma.antonio@proton.me>
2024-09-29 17:51:32 +0200

    Updated .gitignore

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,19 +6,11 @@ function Intro() {
       <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">
         Antonio Di Palma
       </h1>
-      <p className="text-base md:text-xl mb-3 font-medium">Software Engineer</p>
+      <p className="text-base md:text-xl mb-3 font-bold">Software Engineer</p>
       <p className="text-sm max-w-xl mb-6 font-bold">
-        I'm a Software Engineer with a strong passion for technology, coding and
-        video games. After graduating with a bachelor's degree in Computer
-        Engineering, I started my path as a consultant at NTT DATA Italy. I
-        enjoy learning new technologies and experimenting with different tools
-        and frameworks. In addition to my work, I actively contribute and
-        disseminate open-source code, believing in the power of collaboration
-        and knowledge sharing. I have extensive experience in Java, Spring Boot
-        and Java EE, as well as proficiency in ASP.NET, JavaScript, and Python.
-        Proficient in SQL and familiar with various databases. I possess
-        excellent knowledge of Git, TDD, and Agile development methodologies.
-        Additionally, I have a fair understanding of Docker containerization.
+      Java | C# | Spring Boot | Javascript | PostgreSQL | Oracle SQL | Python | HTML | Git <br /> TDD | DDD
+      Azure | Docker | CI/CD | Firebase | Unit Testing | REST | OpenAPI <br /> GitHub Copilot | OOP | WSL
+      Microservices | Distributed Systems | Backend <br /> Italian, English - All professional proficiency or above
       </p>
     </div>
   );
--- src/data/timeline.js
+++ src/data/timeline.js
@@ -3,28 +3,34 @@ export default [
     year: 'Today',
     title: 'Software Engineer',
     duration: '2 years',
-    details: `- Responsible for the maintenance of several monolithic Java Enterprise Edition (Java EE) applications for a public sector client. This role involves ensuring functionality and performance through bug fixes, performance optimization, and security updates, thereby keeping the applications stable and aligned with evolving business needs.
-    - Partecipated in the migration of a legacy SAS application to a modern, scalable ASP.NET REST microservices architecture. This project was designed to support a Backend for Frontend (BFF) system for a client in the energy industry, enhancing system efficiency and responsiveness.
-    - Managed the maintenance and evolution of a Spring Boot microservices backend application, utilizing technologies such as Docker, OpenShift, OpenText, and Azure CI/CD Pipelines. This role focuses on ensuring the continuous improvement and reliability of the REST API services.`,
+    details: `• I spearheaded the design and implementation of new features for enterprise applications, propelling the digital transformation of one of Italy's top three government departments. This was achieved by utilizing a robust stack of technologies including Java, JavaScript, MVC, Oracle SQL, jQuery, and J2EE.
+
+    • Revamped an enterprise application for an energy firm, transitioning from a monolithic structure to a scalable microservices architecture. This enhancement bolstered performance and was instrumental in supporting a revenue stream of €95.6 billion, utilizing technologies such as C#, .NET, Oracle SQL, OpenAPI, along with Test-Driven Development (TDD) and Domain-Driven Design (DDD) methodologies.
+
+    • I developed scalable APIs utilizing the Backend for Frontend (BFF) pattern for an energy firm, aimed at monitoring line interruptions to maintain a stable power supply. This was achieved using technologies including Java, Spring Boot, Azure, OpenAPI, and principles of Test-Driven Development (TDD) and Domain-Driven Design (DDD).
+
+    • Maintained active engagement with clients to comprehend their requirements, converting insights into accurate software specifications that drove successful project execution.
+
+    •  Continuous Integration/Deployment, pull requests, code reviews, load/stress testing, unit testing.`,
   },
   {
     year: '2022',
     title: 'Intern',
     duration: '3 months',
     details:
-      'I was responsible for designing and creating a REST API that signed and exchanged digital certificates between two softwares or virtual machines.',
+      `•   Developed REST APIs (Python) for a Trusted Key Management System utilizing Keylime, an open-source scalable trust system leveraging TPM technology.`
   },
   {
     year: '2019',
     title: 'Bachelor Degree in Computer Engineering',
     duration: '3 years',
-    details: `Passing Grade: 101/110
+    details: `Grade: 101/110
     Thesis: Implementation of a REST API for a Trusted Key Management System in Keylime.`,
   },
   {
     year: '2018',
     title: 'World Class Developer Diploma',
-    duration: '1 years',
+    duration: '1 year',
     details: `The Apple Developer Academy is a partnership between the University of Napoli Federico II and Apple. Training at the Academy is aimed at developing apps for the world’s most innovative and vibrant app ecosystem. The program focuses on software development, startup creation and app design with an emphasis on creativity and collaboration.`,
   },
   {
