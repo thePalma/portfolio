c331417baac8a5fb90d6b5de81aaef492d1f8e03
thePalma <dipalma.antonio@proton.me>
2024-09-29 17:56:25 +0200

    Updated Intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,8 +8,8 @@ function Intro() {
       </h1>
       <p className="text-base md:text-xl mb-3 font-bold">Software Engineer</p>
       <p className="text-sm max-w-xl mb-6 font-bold">
-      Java | C# | Spring Boot | Javascript | PostgreSQL | Oracle SQL | Python | HTML | Git <br /> TDD | DDD
-      Azure | Docker | CI/CD | Firebase | Unit Testing | REST | OpenAPI <br /> GitHub Copilot | OOP | WSL
+      Java | C# | Spring Boot | Javascript | PostgreSQL | Oracle SQL | Python | HTML | Git <br />
+      TDD | DDD | Azure | Docker | CI/CD | Firebase | Unit Testing | REST | OpenAPI <br /> GitHub Copilot | OOP | WSL |
       Microservices | Distributed Systems | Backend <br /> Italian, English - All professional proficiency or above
       </p>
     </div>
